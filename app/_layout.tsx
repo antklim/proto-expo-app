a2dd40de5f80c7fa6e56126ac8794bf68bd4bb57
antklim <antklim@gmail.com>
2023-07-28 12:02:50 +1000

    chore: added comments

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -3,6 +3,11 @@ import { Stack } from 'expo-router'
 // Catch any errors thrown by the Layout component.
 export { ErrorBoundary } from 'expo-router'
 
+// Add initial route here.
+// export const unstable_settings = {
+//   initialRouteName: 'foo bar',
+// }
+
 const RootLayout = () => {
   return (
     <Stack
